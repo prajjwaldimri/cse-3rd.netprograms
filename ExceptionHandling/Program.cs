4c037400612bd56d16f13d03944f1a0f66d80adc
Prajjwal <prajjwaldimri@hotmail.com>
2017-04-28 22:50:22 +0530

    ExceptionHandling

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try 
+            {
+                var zero = 0;
+                //Have to add a variable otherwise the DivideByZeroException
+                //will be thrown at compile time.
+                var a = 199/zero;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally 
+            {
+                Console.WriteLine("Finally Executed");
+            }
+        }
+    }
+}
