8cf9ca82f0aa02801396378ef66dee121c64a1a0
Prajjwal <prajjwaldimri@hotmail.com>
2017-04-28 22:58:19 +0530

    Interfaces

---
+++ Interface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+
+    interface ITest 
+    {
+        void Print();
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ITest test = new Test();
+            test.Print();
+        }
+    }
+
+    public class Test : ITest
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printed");
+        }
+    }
+}
