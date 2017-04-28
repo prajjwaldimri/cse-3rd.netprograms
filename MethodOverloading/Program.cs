3aef1d1b47f0819ef25527c8b85b04a66bb6f3ab
Prajjwal <prajjwaldimri@hotmail.com>
2017-04-28 23:02:22 +0530

    Method Overloading

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var newTest = new Test();
+            newTest.TestMethod();
+            newTest.TestMethod("sample params");
+        }
+    }
+
+    public class Test
+    {
+        public void TestMethod()
+        {
+            Console.WriteLine("Test Method without parameters");
+        }
+
+        public void TestMethod(string param)
+        {
+            Console.WriteLine($"Test method with parameters -> {param}");
+        }
+    }
+}
