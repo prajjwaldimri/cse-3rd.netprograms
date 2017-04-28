dde171253c2e8f3ca397c4aea248873c74aaa236
Prajjwal <prajjwaldimri@hotmail.com>
2017-04-28 22:46:58 +0530

    Type-Conversions

---
+++ Casting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Implicit-Conversion
+            int num = 32000;
+            long bigNum = num;
+            Console.WriteLine(bigNum);
+
+            //Explicit-Conversion
+            double x = 312.123;
+            var a = (int) x;
+            Console.WriteLine(a);
+        }
+    }
+}
