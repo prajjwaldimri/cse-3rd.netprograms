51b72bbad25b736b013e9b67c13ec2cf7306578c
Prajjwal <prajjwaldimri@hotmail.com>
2017-04-28 21:56:13 +0530

    Boxing and Unboxing Program

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 223;
+            // Boxing
+            object o = i;
+            Console.WriteLine(o);
+            // Unboxing
+            o = 256;
+            i = (int) o;
+            Console.WriteLine(i);
+
+        }
+    }
+}
