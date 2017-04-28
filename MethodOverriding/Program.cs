9a796e58af4438de6f0faef180aa004b83693dfd
Prajjwal <prajjwaldimri@hotmail.com>
2017-04-28 23:05:42 +0530

    MethodOverriding

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var derivedClass = new Derived();
+            derivedClass.OverrideThis();
+        }
+    }
+
+    public class Base
+    {
+        public virtual void OverrideThis()
+        {
+            Console.WriteLine("Base Class Method Called");
+        }
+    }
+
+    public class Derived : Base
+    {
+        public override void OverrideThis()
+        {            
+            Console.WriteLine("Derived Class Method Called");
+            base.OverrideThis();
+        }
+    }
+}
